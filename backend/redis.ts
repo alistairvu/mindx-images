e9d13097e56efa847587fff9313c952e0b043715
Alistair Vu <alistairvu@gmail.com>
2021-04-01 23:01:15 +0700

    Edit .env

--- backend/redis.ts
+++ backend/redis.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv"
 
 dotenv.config()
 
-const redisClient = redis.createClient("http://localhost:6379")
+const redisClient = redis.createClient(process.env.REDIS_URL)
 
 redisClient.on("connect", () => {
   console.log("Redis plugged in")

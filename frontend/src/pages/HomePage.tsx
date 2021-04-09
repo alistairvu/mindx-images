c14682dc60ac558c4c17617edd8ec474e90dc3c3
Alistair Vu <alistairvu@gmail.com>
2021-04-09 10:09:46 +0700

    Add Home Page

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,12 +1,49 @@
+import HomeCard from "../components/home/HomeCard"
+import Spinner from "react-bootstrap/Spinner"
 import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import axios from "axios"
+import useSWR from "swr"
 
 const HomePage: React.FC = () => {
+  const getPosts = async () => {
+    try {
+      const { data } = await axios.get("/api/posts")
+      if (data.success) {
+        return data
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const { data: postData } = useSWR("/api/posts", getPosts)
+  console.log(postData)
+
+  if (!postData) {
+    return (
+      <Container className="mt-3 d-flex justify-content-center align-items-center">
+        <Spinner animation="border" />
+      </Container>
+    )
+  }
+
   return (
-    <Container className="text-center py-5">
-      <h1>Welcome to React!</h1>
-      <h3 style={{ fontSize: 30 }}>
-        Get started by editing <code style={{ color: "red" }}>src/App.tsx</code>
-      </h3>
+    <Container className="mt-3">
+      <Row>
+        {postData.posts.map((post: any) => (
+          <Col lg={3} md={6} key={post._id}>
+            <HomeCard
+              createdBy={post.createdBy[0].email}
+              description={post.description}
+              imageUrl={post.imageUrl}
+              title={post.title}
+              id={post._id}
+            />
+          </Col>
+        ))}
+      </Row>
     </Container>
   )
 }

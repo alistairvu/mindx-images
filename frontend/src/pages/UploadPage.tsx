384ac61e40623720b6d73b4cf25c3718b90e8df9
Alistair Vu <alistairvu@gmail.com>
2021-04-15 20:33:45 +0700

    Add pagination

---
+++ frontend/src/pages/UploadPage.tsx
@@ -0,0 +1,11 @@
+import Container from "react-bootstrap/Container"
+
+const UploadPage: React.FC = () => {
+  return (
+    <Container>
+      <h1>Create a new post</h1>
+    </Container>
+  )
+}
+
+export default UploadPage
